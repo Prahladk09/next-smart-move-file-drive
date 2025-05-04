36c5090a1ef7b2cc9307bda05eae8b941b054c88
Prahlad Kumar <prahlad@Mac.lan>
2025-05-05 00:05:04 +0530

    deployement

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,11 +4,15 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import folderRoutes from './routes/folderRoutes';
 import fileRoutes from './routes/fileRoutes';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+
+// for deployemnt
+const _dirname = path.resolve();
 
 // Middleware
 app.use(cors());
@@ -18,6 +22,13 @@ app.use('/uploads', express.static('uploads'));
 // Routes
 app.use('/api/folders', folderRoutes);
 app.use('/api/files', fileRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(_dirname, '../frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // Connect DB and start server
 mongoose.connect(process.env.MONGODB_URI as string).then(() => {
